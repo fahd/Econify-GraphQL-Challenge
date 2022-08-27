eaaba0abaa564481cd3f68a8d4fda4327fa90f1c
Fahd Sheikh <fahdnsheikh@proton.me>
2022-08-27 15:25:14 -0400

    [add] Added into folder structure and basic server file index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+
 const port = process.env.PORT || 9000;
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  graphiql: true,
+}));
+
+app.listen(port);
+console.log(`Running GraphQL API server at: http://localhost:${port}/graphql`);
